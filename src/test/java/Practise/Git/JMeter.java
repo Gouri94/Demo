9e8a1fed77bb1861b08294bdf2d693724a77aea8
Gauri <gouri.soneja@gmail.com>
2020-06-14 17:16:13 +0530

    commit from IST

---
+++ src/test/java/Practise/Git/JMeter.java
@@ -0,0 +1,10 @@
+package Practise.Git;
+
+public class JMeter
+{
+	public void sayJmeter()
+	{
+		System.out.println("Hello JMeter");
+	}
+	
+}
---
+++ src/test/java/Practise/Git/RESTAPI.java
@@ -0,0 +1,10 @@
+package Practise.Git;
+
+public class RESTAPI {
+	
+	public RESTAPI()
+	{
+		System.out.println("RESTAPI");
+	}
+
+}
