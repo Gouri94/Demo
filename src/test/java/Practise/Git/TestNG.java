1e6054d13655acb3b4c2f7bad0b4f39d1cf1da82
Gauri <gouri.soneja@gmail.com>
2020-06-14 18:12:38 +0530

    Commiting.....

---
+++ src/test/java/Practise/Git/TestNG.java
@@ -0,0 +1,15 @@
+package Practise.Git;
+
+public class TestNG {
+	
+	public TestNG()
+	{
+		System.out.println("testng is testing framework.");
+	}
+	
+	public void sayTestNG()
+	{
+		System.out.println("Hello TestNG");
+	}
+
+}
